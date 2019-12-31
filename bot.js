469ad60755527f1efdc737393c1441f47efed5e6
jeonyunsoo123 <42318989+jeonyunsoo123@users.noreply.github.com>
2019-12-31 12:37:06 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 
 
 app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === 'VERIFY_TOKEN') {
+    if (req.query['hub.verify_token'] === 'haegangchat') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong token');
