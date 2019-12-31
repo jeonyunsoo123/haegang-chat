f045df08a4c9ddf4085e7a9a5cf9d720c3941b92
jeonyunsoo123 <jeonyunsoo123@gmail.com>
2019-12-31 14:21:35 +0900

    a

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ school.init(School.Type.HIGH, School.Region.BUSAN, 'C100001908')
 
 //const calendar = school.getCalendar()
 
-const sampleAsync = async function() {
-  const meal = await school.getMeal()
-  const calendar = await school.getCalendar()
+
+  //const meal = await school.getMeal()
+//  const calendar = await school.getCalendar()
 
   // 오늘 날짜
 //  console.log(`${meal.month}월 ${meal.day}일`)
@@ -28,26 +28,16 @@ const sampleAsync = async function() {
   //console.log(calendar)
 
   // 년도와 달을 지정하여 해당 날짜의 데이터를 조회할 수 있습니다.
-  const mealCustom = await school.getMeal(2018, 9)
-  const calendarCustom = await school.getCalendar(2017, 4)
+//  const mealCustom = await school.getMeal(2018, 9)
+//  const calendarCustom = await school.getCalendar(2017, 4)
 
 //  console.log(mealCustom)
 //  console.log(calendarCustom)
 
-  // 년도값 대신 옵션 객체를 전달하여 데이터 수집 가능
-  const optionMeal = await school.getMeal({
-    year: 2018,
-    month: 9,
-    default: '급식이 없습니다'
-  })
-  const optionCalendar = await school.getCalendar({
-    // year, month 생략시 현재 시점 기준으로 조회됨
-    default: '일정 없는 날'
-  })
 
-  //console.log(optionMeal)
-//  console.log(optionCalendar)
+}
 
+sampleAsync()
 //작은 따옴표 사이에 본인이 받으신 token을 paste합니다.
 //나중에 보안을 위해서 따로 setting을 하는 방법을 알려드리겠습니다.
 //이 토큰이 포함된 파일을 절대 업로드하거나 github에 적용시키지 마세요.
@@ -106,12 +96,13 @@ function receivedMessage(event) {
     var senderId = event.sender.id;
     var content = event.message.text;
     var echo_message = "ECHO : " + content;
+    const meal = await school.getMeal()
     //if(content.includes('중식') || content.includes('점심')){
     //sendTextMessage(senderId, school.getMeal());
 
 
 
-   sendTextMessage(senderId, meal.today );
+   sendTextMessage(senderId, meal);
 }
 
 function receivedPostback(event) {
@@ -144,9 +135,7 @@ function sendTextMessage(recipientId, message) {
     });
 }
 
+
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 })
-}
-
-sampleAsync()
