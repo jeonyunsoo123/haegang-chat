655f5d56260fbc0d4c01deb75ce864836dbc130a
jeonyunsoo123 <jeonyunsoo123@gmail.com>
2019-12-31 16:24:15 +0900

    ddddd

--- bot.js
+++ bot.js
@@ -25,26 +25,7 @@ const sampleAsync = async function() {
   // 이번 달 학사일정
   console.log(calendar)
 
-  // 년도와 달을 지정하여 해당 날짜의 데이터를 조회할 수 있습니다.
-  const mealCustom = await school.getMeal(2018, 9)
-  const calendarCustom = await school.getCalendar(2017, 4)
 
-  console.log(mealCustom)
-  console.log(calendarCustom)
-
-  // 년도값 대신 옵션 객체를 전달하여 데이터 수집 가능
-  const optionMeal = await school.getMeal({
-    year: 2018,
-    month: 9,
-    default: '급식이 없습니다'
-  })
-  const optionCalendar = await school.getCalendar({
-    // year, month 생략시 현재 시점 기준으로 조회됨
-    default: '일정 없는 날'
-  })
-
-  console.log(optionMeal)
-  console.log(optionCalendar)
 }
 
 sampleAsync()
